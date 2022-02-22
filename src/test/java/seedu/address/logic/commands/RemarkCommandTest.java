8af9f8b3f7b3e26a5aa4475c3a7da0352ba8e41f
Ye Yint Paing Oo <e0543517@u.nus.edu>
2022-02-22 19:31:35 +0800

    Update RemarkCommandTest

--- src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
+++ src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
@@ -104,26 +104,4 @@ public class RemarkCommandTest {
 
         assertCommandFailure(remarkCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
     }
-
-    @Test
-    public void equals() {
-        final RemarkCommand standardCommand = new RemarkCommand(INDEX_FIRST_PERSON,
-                new Remark(VALID_REMARK_AMY));
-        // same values -> returns true
-        RemarkCommand commandWithSameValues = new RemarkCommand(INDEX_FIRST_PERSON,
-                new Remark(VALID_REMARK_AMY));
-        assertTrue(standardCommand.equals(commandWithSameValues));
-        // same object -> returns true
-        assertTrue(standardCommand.equals(standardCommand));
-        // null -> returns false
-        assertFalse(standardCommand.equals(null));
-        // different types -> returns false
-        assertFalse(standardCommand.equals(new ClearCommand()));
-        // different index -> returns false
-        assertFalse(standardCommand.equals(new RemarkCommand(INDEX_SECOND_PERSON,
-                new Remark(VALID_REMARK_AMY))));
-        // different remark -> returns false
-        assertFalse(standardCommand.equals(new RemarkCommand(INDEX_FIRST_PERSON,
-                new Remark(VALID_REMARK_BOB))));
-    }
 }
